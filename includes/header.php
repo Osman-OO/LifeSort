e116005b5c318b558e9d6b96b2daee3ebae20490
Osman-OO <osmandabdullahi@gmail.com>
2025-07-07 14:00:43 -0400

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,9 @@
-<?php
-// Common header and navigation for Liferoll project
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>LifeRoll</title>
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/animations.css">
+</head>
+<body>
