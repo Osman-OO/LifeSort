c46acf1a0c30d8b3e998fe59333a9927a5595f44
Osman-OO <osmandabdullahi@gmail.com>
2025-07-07 14:10:59 -0400

    Update process_choice.php

--- screens/process_choice.php
+++ screens/process_choice.php
@@ -1,1 +1,25 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $career = $_POST['career'];
+  
+  switch ($career) {
+    case 'college':
+      $_SESSION['wealth'] -= 5000;
+      $_SESSION['education'] = 'college';
+      break;
+    case 'army':
+      $_SESSION['wealth'] += 2000;
+      $_SESSION['education'] = 'army';
+      break;
+  }
 
+  // Check for bankruptcy
+  if ($_SESSION['wealth'] <= 0) {
+    header("Location: gameover.php");
+    exit;
+  }
+
+  header("Location: roll.php"); // Proceed to dice roll
+}
+?>
