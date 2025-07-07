f195175d956bbccceb5926ac2186c027702feb50
Osman-OO <osmandabdullahi@gmail.com>
2025-07-07 13:55:22 -0400

    Update index.php

--- screens/index.php
+++ screens/index.php
@@ -1,3 +1,16 @@
-<?php
-// Homepage (main menu) for Liferoll project
-?>
+<?php include 'includes/header.php'; ?>
+
+<div class="main-menu">
+  <h1>LifeRoll: Wealth & Career Odyssey</h1>
+  <img src="assets/images/game-logo.png" alt="Game Logo">
+  <div class="rules">
+    <h2>Rules:</h2>
+    <p>Start with random wealth. Reach retirement without going broke!</p>
+  </div>
+  <div class="buttons">
+    <a href="leaderboard.php" class="btn">Leaderboard</a>
+    <a href="board1.php" class="btn">Start Game</a>
+  </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
