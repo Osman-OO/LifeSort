85c8b1f91ccaeda858a1e15e8de27e9b161d90e9
Osman-OO <osmandabdullahi@gmail.com>
2025-07-07 14:05:31 -0400

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,14 @@
 <?php
-// Leaderboard database connection and functions (SQLite or file-based)
+function save_score($name, $age, $wealth) {
+  $data = "$name,$age,$wealth\n";
+  file_put_contents("leaderboard.txt", $data, FILE_APPEND);
+}
+
+function get_leaderboard() {
+  if (!file_exists("leaderboard.txt")) return [];
+  $scores = file("leaderboard.txt");
+  return array_map(function($line) {
+    return explode(',', trim($line));
+  }, $scores);
+}
 ?>
