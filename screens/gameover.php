becbac16fea4d8dee9e89823b203cd59fa310c03
Osman-OO <osmandabdullahi@gmail.com>
2025-07-07 14:22:29 -0400

    Update gameover.php

--- screens/gameover.php
+++ screens/gameover.php
@@ -1,3 +1,9 @@
-<?php
-// Lose condition screen for Liferoll project
-?>
+<?php include 'includes/header.php'; ?>
+
+<div class="gameover">
+  <h1>GAME OVER</h1>
+  <p>You ran out of money at age <?php echo $_SESSION['age']; ?>!</p>
+  <a href="index.php" class="btn">Try Again</a>
+</div>
+
+<?php include 'includes/footer.php'; ?>
