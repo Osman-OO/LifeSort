7fad4eb86afa6c960a147b7da059e4441c37e6d8
Osman-OO <osmandabdullahi@gmail.com>
2025-07-07 14:55:16 -0400

    Update board2.php

--- screens/board2.php
+++ screens/board2.php
@@ -1,12 +1,3 @@
-<?php
-session_start();
-// Redirect if player hasn't reached this board yet
-if ($_SESSION['position'] < 10) {
-  header("Location: board1.php");
-  exit;
-}
-?>
-
 <?php include 'includes/header.php'; ?>
 
 <div class="board">
@@ -15,24 +6,26 @@ if ($_SESSION['position'] < 10) {
   <p>Age: <?php echo $_SESSION['age']; ?></p>
   <p>Position: <?php echo $_SESSION['position']; ?></p>
 
-  <form method="post" action="process_midlife.php">
-    <h3>Choose an Action:</h3>
+  <form method="post" action="process_choice.php">
+    <h3>Life Events:</h3>
     <label>
-      <input type="radio" name="action" value="invest"> 
-      Invest in Stocks (Risk: Lose $3000 or Gain $7000)
+      <input type="radio" name="action" value="invest" required> 
+      Invest in Stocks (50%: +$7,000 or -$3,000)
     </label><br>
     <label>
       <input type="radio" name="action" value="marry"> 
-      Get Married (Cost: $2000)
+      Get Married (-$2,000)
     </label><br>
     <label>
       <input type="radio" name="action" value="job"> 
-      Change Jobs (+$4000/year)
+      Change Jobs (+$4,000)
     </label><br>
-    <button type="submit">Submit</button>
+    <label>
+      <input type="radio" name="action" value="lottery"> 
+      Play Lottery (10%: +$10,000)
+    </label><br>
+    <button type="submit" class="btn">Submit</button>
   </form>
-
-  <a href="roll.php" class="btn">Roll Dice to Move</a>
 </div>
 
 <?php include 'includes/footer.php'; ?>
