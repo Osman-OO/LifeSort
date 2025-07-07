b97471386808c70f51d1d698c10278a5bb855942
Osman-OO <osmandabdullahi@gmail.com>
2025-07-07 14:27:02 -0400

    Update win.php

--- screens/win.php
+++ screens/win.php
@@ -2,20 +2,17 @@
 session_start();
 include 'includes/db.php';
 
-// Save to leaderboard
+// Save score to leaderboard
 save_score("Player", $_SESSION['age'], $_SESSION['wealth']);
 
-// Reset session
+// Reset game (optional)
 session_destroy();
 ?>
 
 <?php include 'includes/header.php'; ?>
-
-<div class="win">
-  <h1>CONGRATULATIONS!</h1>
-  <p>You retired at age <?php echo $_SESSION['age']; ?> with $<?php echo $_SESSION['wealth']; ?>!</p>
-  <a href="leaderboard.php" class="btn">View Leaderboard</a>
-  <a href="index.php" class="btn">Play Again</a>
+<div class="win-screen">
+  <h1>You Won!</h1>
+  <p>Retired at age <?php echo $_SESSION['age']; ?> with $<?php echo $_SESSION['wealth']; ?>.</p>
+  <a href="leaderboard.php" class="btn">Leaderboard</a>
 </div>
-
 <?php include 'includes/footer.php'; ?>
